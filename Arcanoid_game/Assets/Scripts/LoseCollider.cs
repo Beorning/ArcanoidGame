6abda32843b282b597513adb8d2d2ce19a90d95b
Beorning <ibahun2@gmail.com>
2019-05-07 17:21:22 +0200

    Added walls, game over and paddle stering

---
+++ Arcanoid_game/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("kupa");
+        Debug.Log("Level load requested for: " + "Lose");
+        SceneManager.LoadScene("Lose");
+    }
+
+
+}
