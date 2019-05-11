bd03aeccd66b9efd27fcc21c5e3eadc5a8307ed1
Beorning <ibahun2@gmail.com>
2019-05-11 18:29:13 +0200

    added ball script (ball glued to paddle) and updated paddle script to restrain it's movement.

--- Arcanoid_game/Assets/Scripts/Paddle.cs
+++ Arcanoid_game/Assets/Scripts/Paddle.cs
@@ -6,12 +6,15 @@ public class Paddle : MonoBehaviour
 {
     
     [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
 
     
     void Update()
     {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
               
           
