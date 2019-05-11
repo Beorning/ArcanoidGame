03ce43f9fa256e105df6ecee8752f2f5858cf7ea
Beorning <ibahun2@gmail.com>
2019-05-11 19:38:45 +0200

    added Ball launcher script and block destroyer

--- Arcanoid_game/Assets/Scripts/Ball.cs
+++ Arcanoid_game/Assets/Scripts/Ball.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
-
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    [SerializeField] Rigidbody2D rb;
     Vector2 paddleToBallVector;
 
 
@@ -21,6 +25,27 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
---
+++ Arcanoid_game/Assets/Scripts/Block.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject, 0);
+        Debug.Log(collision.gameObject.name);
+    }
+
+
+
+
+
+
+}
